bdc05a0d4fcb554b3b1117fe5c9657532bbb8497
Matthew Forsyth <matt@mojotech.com>
2026-03-15 15:53:40 -0400

    Minimalistic splash page.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,26 +2,14 @@ import React from "react";
 
 export const STRINGS = {
   BRANDING: {
-    SITE_TITLE: ["dr4ft", "info"].join("."),
-    SITE_NAME: <span>DR<img src='4.png' alt='4'/>FT</span>,
-    DEFAULT_USERNAME: "dr4fter",
+    SITE_TITLE: "Warmachine Leader Draft",
+    SITE_NAME: <span>Warmachine Leader Draft</span>,
+    DEFAULT_USERNAME: "drafter",
     PAYPAL: "",
   },
 
   PAGE_SECTIONS: {
-    MOTD: null, // TODO: handle overwrite of this message of the day; can be a React element
-
-    FOOTER:
-      <div style={{ padding: 10 }}>
-        Contributions welcome! &nbsp;
-        <a href='https://github.com/dr4fters/dr4ft' target='_blank' rel='noreferrer'>
-          <img
-            src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg'
-            alt='GitHub' title='GitHub Repository' align='top' height='18'
-            style={{ marginRight: 5}}
-          />
-          dr4fters/dr4ft
-        </a>
-      </div>
+    MOTD: null,
+    FOOTER: null
   }
 };
--- frontend/src/lobby/Lobby.jsx
+++ frontend/src/lobby/Lobby.jsx
@@ -6,8 +6,6 @@ import {STRINGS} from "../config";
 import Header from "./Header";
 import GamesPanel from "./GamesPanel";
 import NewsPanel from "./NewsPanel";
-import FileUpload from "./FileUpload";
-import Version from "./Version";
 
 export default class Lobby extends Component {
 
@@ -17,18 +15,14 @@ export default class Lobby extends Component {
   }
   render() {
     document.title = STRINGS.BRANDING.SITE_TITLE;
-    const { roomInfo, serverVersion, mtgJsonVersion, boosterRulesVersion } = App.state;
+    const { roomInfo } = App.state;
 
     return (
       <div className="container">
         <div className="lobby">
           <Header/>
           <GamesPanel roomInfo={roomInfo}/>
-          <FileUpload />
           {STRINGS.PAGE_SECTIONS.MOTD && <NewsPanel motd={STRINGS.PAGE_SECTIONS.MOTD}/>}
-          {STRINGS.BRANDING.PAYPAL}
-          {STRINGS.PAGE_SECTIONS.FOOTER}
-          <Version version={serverVersion} MTGJSONVersion={mtgJsonVersion} boosterRulesVersion={boosterRulesVersion}/>
         </div>
       </div>
     );
